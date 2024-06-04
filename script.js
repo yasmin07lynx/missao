0e365ead86c045796ef4cf84e92b5153ff0c82a5
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-06-04 11:49:26 -0300

    .

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelectorAll(".caixa-principal");
+const caixaPergunta = document.querySelectorAll(".caixa-pergunta");
+const caixaAlternativa = document.querySelectorAll(".caixa-alternativa");
+const caixaResultado = document.querySelectorAll(".caixa-resultado");
+const caixaPrincipal = document.querySelectorAll(".caixa-principal");
