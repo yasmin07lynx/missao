039ac7e8e06d3104b3a6b2db7939d0e39ee076f0
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-06-04 11:58:32 -0300

    .lkjhgfdasdfrgthyjuikolçp[a

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ const caixaPrincipal = document.querySelectorAll(".caixa-principal");
 const caixaPergunta = document.querySelectorAll(".caixa-pergunta");
 const caixaAlternativa = document.querySelectorAll(".caixa-alternativa");
 const caixaResultado = document.querySelectorAll(".caixa-resultado");
-const caixaPrincipal = document.querySelectorAll(".caixa-principal");
+const textoResultado = document.querySelectorAll(".texto-resultado");
+
+const perguntas = [ 
+    { //primeiro objeto da lista de perguntas
+        enunciado: "pergunta1", alternativas: ["alternativa1", "alternativa2"]    
+    },
+    { //segundo objeto da lista de perguntas
+         enunciado: "pergunta1", alternativas: ["alternativa1", "alternativa2"]
+
+    }
+
+];
