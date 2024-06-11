e5147bfbaccdccfe9199bc2fa5fb188b2b74f818
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-06-11 12:02:31 -0300

    çlkjhgf

--- script.js
+++ script.js
@@ -4,15 +4,28 @@ const caixaAlternativa = document.querySelectorAll(".caixa-alternativa");
 const caixaResultado = document.querySelectorAll(".caixa-resultado");
 const textoResultado = document.querySelectorAll(".texto-resultado");
 
-const perguntas = [ 
-    { //primeiro objeto da lista de perguntas
-        enunciado: "pergunta1", 
-        alternativas: ["alternativa1", "alternativa2"]    
+const perguntas = [
+    {
+        enunciado: "Qual a melhor maneira de combater o desmatamento?", 
+        alternativas:[
+            "Reflorestamento de área degradadas", 
+            "Implementação de leis mais rigorosas para a proteção das florestas"
+        ]
     },
-    { //segundo objeto da lista de perguntas
-         enunciado: "pergunta2", 
-         alternativas: ["alternativa1", "alternativa2"]
-
+    {
+        enunciado: "Como podemos combater a desigualdade social de forma eficaz e sustentável?", 
+        alternativas:[
+            "Implementar programas de educação e formação profissional acessíveis a todos, capacitando indivíduos de comunidades desfavorecidas.", 
+            "políticas de redistribuição de renda e oportunidades,ppromovendo uma distribuição igual dos recursos e do acesso aos benefícios"
+        ]
+    },
+    ];
+    
+    let atual = 0;
+    let perguntaAtual;
+    
+    function mostraPergunta(){
+        perguntaAtual = perguntas[atual];
+        caixaPerguntas.textContent = perguntaAtual.enunciado;
     }
-
-];
+    mostraPergunta();
