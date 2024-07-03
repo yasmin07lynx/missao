d113f630d9f8ff2bdd542d0641c7a37d3f795dfb
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-07-03 12:05:43 -0300

    JDFG

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
         enunciado: "Qual a melhor maneira de combater o desmatamento?", 
-        alternativas:[
+        alternativa:[
             "Reflorestamento de área degradadas", 
             "Implementação de leis mais rigorosas para a proteção das florestas"
         ]
     },
     {
         enunciado: "Como podemos combater a desigualdade social de forma eficaz e sustentável?", 
-        alternativas:[
+        alternativa:[
             "Implementar programas de educação e formação profissional acessíveis a todos, capacitando indivíduos de comunidades desfavorecidas.", 
             "políticas de redistribuição de renda e oportunidades,ppromovendo uma distribuição igual dos recursos e do acesso aos benefícios"
         ]
@@ -25,7 +25,7 @@ const perguntas = [
     let perguntaAtual;
     
     function mostraPergunta(){
-        perguntaAtual = perguntas[atual];
+        perguntaAtual = pergunta[atual];
         caixaPergunta.textContent = perguntaAtual.enunciado;
     }
     mostraPergunta();
